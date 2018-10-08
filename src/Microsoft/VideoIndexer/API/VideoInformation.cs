b5d59a7c4faec11bf1a762f2c17bbdc624c00f6a
Murillo Grubler <msgrubler@gmail.com>
2018-10-07 22:39:52 -0300

    Updating video indexer code

--- src/Microsoft/VideoIndexer/API/VideoInformation.cs
+++ src/Microsoft/VideoIndexer/API/VideoInformation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -11,15 +12,61 @@ namespace VideoIndexer.Api
 {
     internal class VideoInformation
     {
-        private readonly string _url;
-        private readonly string _token;
+        private readonly string _apiUrl;
+        private readonly string _apiKey;
+        private readonly string _location;
+        private readonly string _accountId;
 
-        public VideoInformation(string url, string token)
+        public VideoInformation(string apiKey, string apiUrl, string location, string accountId)
         {
-            _url = url;
-            _token = token;
+            _apiKey = apiKey;
+            _apiUrl = apiUrl;
+            _location = location;
+            _accountId = accountId;
         }
 
-        
+        public string Process(string videoUrl)
+        {
+            var handler = new HttpClientHandler(); 
+            handler.AllowAutoRedirect = false; 
+            using (var client = new HttpClient(handler))
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey); 
+
+                // obtain account access token
+                var accountAccessTokenRequestResult = client.GetAsync($"{_apiUrl}/auth/{_location}/Accounts/{_accountId}/AccessToken?allowEdit=true").Result;
+                var accountAccessToken = accountAccessTokenRequestResult.Content.ReadAsStringAsync().Result.Replace("\"", "");
+                client.DefaultRequestHeaders.Remove("Ocp-Apim-Subscription-Key");
+
+                // upload a video
+                var content = new MultipartFormDataContent();
+                var uploadRequestResult = client.PostAsync($"{_apiUrl}/{_location}/Accounts/{_accountId}/Videos?accessToken={accountAccessToken}&name=some_name&description=some_description&privacy=private&partition=some_partition&videoUrl={videoUrl}", content).Result;
+                var uploadResult = uploadRequestResult.Content.ReadAsStringAsync().Result;
+
+                // get the video id from the upload result
+                var returnInfos = JsonConvert.DeserializeObject<dynamic>(uploadResult);
+                var returnInfosString = returnInfos.ToString();
+
+                return returnInfos["id"].ToString();
+            }
+        }
+
+        public Task<string> StatusProcess(string videoId)
+        {
+            var handler = new HttpClientHandler(); 
+            handler.AllowAutoRedirect = false; 
+            using (var client = new HttpClient(handler))
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+                var videoTokenRequestResult = client.GetAsync($"{_apiUrl}/auth/{_location}/Accounts/{_accountId}/Videos/{videoId}/AccessToken?allowEdit=true").Result;
+                var videoAccessToken = videoTokenRequestResult.Content.ReadAsStringAsync().Result.Replace("\"", "");
+
+                client.DefaultRequestHeaders.Remove("Ocp-Apim-Subscription-Key");
+
+                var videoGetIndexRequestResult = client.GetAsync($"{_apiUrl}/{_location}/Accounts/{_accountId}/Videos/{videoId}/Index?accessToken={videoAccessToken}&language=English").Result;
+                var videoGetIndexResult = videoGetIndexRequestResult.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<dynamic>(videoGetIndexResult)["state"].ToString();
+            }
+        }
     }
 }
--- src/Microsoft/VideoIndexer/Controllers/HomeController.cs
+++ src/Microsoft/VideoIndexer/Controllers/HomeController.cs
@@ -4,29 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VideoIndexer.Api;
 using VideoIndexer.Models;
 
 namespace VideoIndexer.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+             // That variables will load key and url where will be processed from Microsoft
+            var apiKey = "";
+            var apiUrl = "";
+            var location = "";
+            var accountId = "";
+            var video = "https://images.all-free-download.com/footage_preview/mp4/horses_101.mp4";
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+            // This code predict informations concern image from API Microsoft
+            var videoIndexer = new VideoInformation(apiKey, apiUrl, location, accountId);
+            var result = videoIndexer.Process(video);
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            // Return a view and the object that will be processed
+            return View("Index");
         }
 
         public IActionResult Error()
