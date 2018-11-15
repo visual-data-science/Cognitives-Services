912e7761bce89e5a4ba8ce66cf1af51b21fca96b
Murillo Grubler <msgrubler@gmail.com>
2018-11-15 20:23:54 -0200

    Adjusting the Insights classes.

--- src/Microsoft/VideoIndexer/API/Label.cs
+++ src/Microsoft/VideoIndexer/API/Label.cs
@@ -5,7 +5,9 @@ namespace VideoIndexer.Api
     public sealed class Label
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+        
         public List<Time> Appearances { get; set; }
     }
 }
--- src/Microsoft/VideoIndexer/API/Time.cs
+++ src/Microsoft/VideoIndexer/API/Time.cs
@@ -3,8 +3,11 @@ namespace VideoIndexer.Api
     public sealed class Time
     {
         public string StartTime { get; set; }
+
         public string EndTime { get; set; }
+
         public decimal StartSeconds { get; set; }
+        
         public decimal EndSeconds { get; set; }
     }
 }
